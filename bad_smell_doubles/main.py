99382723032af7d2e66cc6a2c5ca4231bc74e594
Viktor A Orlov <121401003+va0r@users.noreply.github.com>
2023-03-29 14:30:00 +0300

    Add files via upload

---
+++ bad_smell_doubles/main.py
@@ -0,0 +1,33 @@
+class SomeClass:
+    def __init__(self):
+        self.lst = [3, 2, 1, 4, 2, 1]
+
+    def sorted_func(self):
+        return sorted(self.lst, reverse=False)
+
+
+if __name__ == "__main__":
+    some_inst = SomeClass()
+    print(some_inst.sorted_func())
+
+# # Как то вечером три разработчика написали
+# # три метода класса `SomeClass`, каждый под себя.
+# # Методы по сути своей почти одинаковые.
+# #
+# # Напишите свой метод `sorted_func`,
+# # учитывая особенности всех представленных методов
+#
+#
+# class SomeClass:
+#     def __init__(self):
+#         self.lst = [3, 2, 1, 4, 2, 1]
+#
+#     def sorted(self):
+#         self.lst.sort()
+#         return self.lst
+#
+#     def sorting(self):
+#         return sorted(self.lst)
+#
+#     def asc_sorting(self):
+#         return sorted(self.lst, reverse=False)
---
+++ bad_smell_envious_func/main.py
@@ -0,0 +1,44 @@
+class Cube:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def get_volume(self):
+        return self.x * self.y * self.z
+
+
+class CubeVolumeCalculator:
+
+    @staticmethod
+    def calc_cube_volume(cube):
+        return cube.get_volume()
+
+# # Вам не кажется, что CubeVolumeCalculator
+# # чаще дергает методы класса Cube? Исправьте так,
+# # чтобы избавиться от лишних обращений к классу Cube
+#
+#
+# class Cube:
+#
+#     def __init__(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#
+#     def get_x(self):
+#         return self.x
+#
+#     def get_y(self):
+#         return self.y
+#
+#     def get_z(self):
+#         return self.z
+#
+#
+# class CubeVolumeCalculator:
+#
+#     @staticmethod
+#     def calc_cube_volume(cube):
+#         return cube.get_x() * cube.get_y() * cube.get_z()
